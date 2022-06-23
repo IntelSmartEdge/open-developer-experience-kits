e33837157ff9b8f255be946e6c24e51bc76d8c88
Sunil Kumar Parida <sunil.kumar.parida@intel.com>
2022-06-23 03:32:47 +0000

    Smart Edge Open 22.03.01 release

--- roles/telemetry/collectd/node/files/write_notification.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (c) 2020 Intel Corporation
-
-rm -f /tmp/notifications
-while read -r x y
-do
-  echo "$x$y" >> /tmp/notifications
-done
